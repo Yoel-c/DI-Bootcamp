225479d32c287a4967ed27a2aa99a0ae90056007
EmmaYoel <cohenyoel2@gmail.com>
2023-07-25 11:04:38 +0300

    week3day2dailychalenge2507

---
+++ Week3/Day2/DailyChallenge/index.js
@@ -0,0 +1,24 @@
+//Exercice 1
+
+
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift()
+
+fruits.sort()
+
+fruits.push('kiwi')
+
+fruits.splice(0 , 1)
+
+fruits.reverse()
+
+console.log(fruits);
+
+
+//Exercice2
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1][0]);
+
